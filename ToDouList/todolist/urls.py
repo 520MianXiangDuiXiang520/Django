e122a0b5c7118161bf747f5366f71620f3637c4e
520MianXiangDuiXiang520 <15364968962@163.com>
2019-01-20 15:50:07 +0800

    通过数据库存储

--- ToDouList/todolist/urls.py
+++ ToDouList/todolist/urls.py
@@ -5,8 +5,8 @@ app_name="todolist"
 urlpatterns = [
     path('home/',views.home,name="主页"),
     path('about/',views.about,name="关于"),
-    path('edit/<forloop_counter>',views.edit,name="编辑"),
-    path('del/<forloop_counter>',views.delete,name="删除"),
-    path('success/<forloop_counter>', views.success, name="划掉"),
-    path('unsuccess/<forloop_counter>', views.unsuccess, name="撤销"),
+    path('edit/<everything_id>',views.edit,name="编辑"),
+    path('del/<everything_id>',views.delete,name="删除"),
+    path('success/<everything_id>', views.success, name="划掉"),
+    path('unsuccess/<everything_id>', views.unsuccess, name="撤销"),
 ]
--- ToDouList/todolist/views.py
+++ ToDouList/todolist/views.py
@@ -1,45 +1,47 @@
 from django.shortcuts import render,redirect
-
+from .models import Todo
 # Create your views here.
-id=1
-lst=[
-    {'事件名称':'遛狗','完成情况':False},
-    {'事件名称':'pp','完成情况':True},
-]
+
 
 def home(request):
-    global lst
     if request.method=="POST":
         if request.POST['待办']=='':
-            connect = {'警告':'内容不可以为空！','清单': lst}
+            connect = {'警告':'内容不可以为空！','清单': Todo.objects.all()}
             return render(request, "todolist/home.html", connect)
         else:
-            lst.append({'事件名称': request.POST['待办'], '完成情况': False})
-            connect={'成功提示':'待办添加成功！','清单': lst}
+            a_row=Todo(thing=request.POST['待办'], done=False)
+            a_row.save()
+            connect={'成功提示':'待办添加成功！','清单': Todo.objects.all()}
             return render(request,"todolist/home.html",connect)
     else:
-        connect = {'清单': lst}
+        connect = {'清单': Todo.objects.all()}
         return render(request, "todolist/home.html", connect)
 
 def about(request):
     return render(request,"todolist/about.html")
 
-def edit(request,forloop_counter):
+def edit(request,everything_id):
+    a = Todo.objects.get(id=int(everything_id))
     if request.method=="POST":
-        connect={'事件名称': request.POST['change'], '完成情况': False}
-        lst[int(forloop_counter)-1]=connect
+        a.thing=request.POST['change']
+        a.save()
         return redirect("todolist:主页")
     else:
-        return render(request, "todolist/edit.html",{'事件名称': lst[int(forloop_counter)-1]['事件名称']})
+        return render(request, "todolist/edit.html",{'事件名称':a.thing})
 
-def delete(request,forloop_counter):
-    lst.pop(int(forloop_counter)-1)
+def delete(request,everything_id):
+    a=Todo.objects.get(id=everything_id)
+    a.delete()
     return redirect("todolist:主页")
 
-def success(request,forloop_counter):
-    lst[int(forloop_counter)-1]['完成情况']=True
+def success(request,everything_id):
+    a = Todo.objects.get(id=int(everything_id))
+    a.done=True
+    a.save()
     return redirect("todolist:主页")
 
-def unsuccess(request,forloop_counter):
-    lst[int(forloop_counter)-1]['完成情况']=False
+def unsuccess(request,everything_id):
+    a = Todo.objects.get(id=int(everything_id))
+    a.done = False
+    a.save()
     return redirect("todolist:主页")
