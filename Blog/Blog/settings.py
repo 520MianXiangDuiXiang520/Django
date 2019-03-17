cb5e6a9d88e975db174e2fb6d5add2abf7160987
520MianXiangDuiXiang520 <15364968962@163.com>
2019-03-17 18:39:54 +0800

    Nginx，Gunicorn部署博客到Linux服务器

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'kyfhkc+7tg14tj#+2a%_^k2*m0s1!qd2k-$j+=@7y6+y@8b69!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost','120.78.178.114']
 
 
 # Application definition
@@ -123,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
